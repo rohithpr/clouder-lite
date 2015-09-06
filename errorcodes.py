cc498703e2d8fd4932a6a3027b3f89a0ae0dbe41
rohithpr <praroh2@gmail.com>
2015-09-06 20:19:43 +0530

    Added error codes

---
+++ errorcodes.py
@@ -0,0 +1,8 @@
+"""
+List of errors that will be returned to the client.
+Add any new error codes only to the end of list. Do not alter existing codes.
+"""
+
+codes = {
+	'1' - 'Method should be POST',
+}
